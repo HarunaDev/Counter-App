dadc5c91f83263de94ea84f0217492871beddb3d
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-22 13:11:52 +0100

    added data for app to track using state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
    render() {
     return (<div>Hello</div>)
    }
