5bb7e9bc6a3cc95e0b1a9c58ea57532d30540f89
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-22 13:20:34 +0100

    created buttons in app component and rendered value of count in display

--- src/App.js
+++ src/App.js
@@ -10,8 +10,16 @@ class App extends React.Component{
     }
   }
    render() {
-    return (<div>Hello</div>)
-   }
+    return ( 
+    <div>
+      <button className='inc' onClick={this.increment}>Increment</button>
+      <button className='dec' onClick={this.decrement}>Decrement</button>
+      <button className='res' onClick={this.reset}>Reset</button>
+
+      <h1>Current Count: {this.state.count}</h1>
+    </div>
+    )
+  }
 }
 
 export default App;
